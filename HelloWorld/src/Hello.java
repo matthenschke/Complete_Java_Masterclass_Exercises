876f9cd0a084516f5580a3d0a5add44058693e01
Matthew Henschke <matthewhenschke98@gmail.com>
2018-09-27 09:32:27 -0400

    Add files via upload

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * First IntelliJ Program using Java 11
+ */
+
+public class Hello {
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        int num = 5;
+        System.out.println(num);
+    }
+}
