da9835510ab078195e2f9a64e2fc3fea67772caf
Matthew Henschke <matthewhenschke98@gmail.com>
2018-10-08 19:23:08 -0400

    char and boolean exercise

---
+++ CharAndBoolean/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    /*
+    // width of 16 (2 bytes)
+	char myChar = '\u00A9';
+	System.out.println(myChar);
+
+	boolean myBoolean = false;
+	boolean isMale = true;
+	*/
+
+    // 1. Find the code for the registered symbol on the same line as the copyright symbol
+    // 2. Create a variable of type char and assign it the unicode value for that symbol.
+    // 3. Display it on the screen
+
+    char registerSymbol = '\u00AE';
+    System.out.println(registerSymbol);
+    }
+}
